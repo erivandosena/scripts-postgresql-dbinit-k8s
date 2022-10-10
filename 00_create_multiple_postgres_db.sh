a0eb1b9630721ddb8046925604da6ce56b612be3
Erivando Sena <erivandosena@gmail.com>
2022-10-09 22:29:47 -0300

    Update 00_create_multiple_postgres_db.sh

--- 00_create_multiple_postgres_db.sh
+++ 00_create_multiple_postgres_db.sh
@@ -10,9 +10,9 @@ set -u
 cmd="$@"
 timer="5"
 
-until runuser -l postgres -c 'pg_isready' 2>/dev/null; do
-    >&2 echo "Postgres is unavailable - sleeping for $timer seconds"
-    sleep $timer
+until runuser -l postgresql -c 'pg_isready' 2>/dev/null; do
+  >&2 echo "PostgreSQL não está disponível, aguardando $timer seconds"
+  sleep $timer
 done
 
 >&2 echo "Postgres is up - executing command"
@@ -28,28 +28,28 @@ echo " ⏳ Configurando o PostgreSQL"
 
 # databases
 function create_user_and_database() {
-    local database=$1
-    echo " ⏳ Criando usuário e banco de dados '$database'"
-    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=postgres user=$POSTGRESQL_SUPERADMIN_USER" -W "$POSTGRESQL_SUPERADMIN_PASSWORD" <<-EOSQL
-        
-        --CREATE USER IF NOT EXISTS $POSTGRES_USER WITH PASSWORD $POSTGRES_PASSWORD;
-        ALTER USER $POSTGRES_USER WITH CREATEDB CREATEROLE REPLICATION SUPERUSER;
-        CREATE DATABASE $database;
-        GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+  local database=$1
+  echo " ⏳ Criando usuário e banco de dados '$database'"
+  psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=postgres user=$POSTGRESQL_SUPERADMIN_USER" -W "$POSTGRESQL_SUPERADMIN_PASSWORD" <<-EOSQL
+
+      --CREATE USER IF NOT EXISTS $POSTGRESQL_USERNAME WITH PASSWORD $POSTGRESQL_PASSWORD;
+      ALTER USER $POSTGRESQL_USERNAME WITH CREATEDB CREATEROLE REPLICATION SUPERUSER;
+      CREATE DATABASE $database;
+      GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRESQL_USERNAME;
 EOSQL
 }
 
 # schemas
 function create_schemas() {
-    local database=$1
-    echo " ⏳ Criando schemas para o banco de dados '$database'"
-    # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --password "$POSTGRES_PASSWORD" -d "$database" <<-EOSQL
-    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" <<-EOSQL
-        
+  local database=$1
+  echo " ⏳ Criando schemas para o banco de dados '$database'"
+  # psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USERNAME" --password "$POSTGRESQL_PASSWORD" -d "$database" <<-EOSQL
+    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" <<-EOSQL
+
         CREATE SCHEMA IF NOT EXISTS dbo;
         CREATE SCHEMA IF NOT EXISTS "ProgramaGestao";
-        GRANT ALL ON SCHEMA dbo TO $POSTGRES_USER;
-        GRANT ALL ON SCHEMA "ProgramaGestao" TO $POSTGRES_USER;
+        GRANT ALL ON SCHEMA dbo TO $POSTGRESQL_USERNAME;
+        GRANT ALL ON SCHEMA "ProgramaGestao" TO $POSTGRESQL_USERNAME;
         SET search_path TO dbo;
         SET search_path TO "ProgramaGestao";
         ALTER DATABASE $database SET search_path TO dbo;
@@ -60,11 +60,11 @@ EOSQL
 
 # migrations
 function create_migrations() {
-    local database=$1
-    echo " ⏳ Excluindo schema padrão."
-    # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --password "$POSTGRES_PASSWORD" -d "$database" <<-EOSQL
-    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" <<-EOSQL
-        
+  local database=$1
+  echo " ⏳ Excluindo schema padrão."
+  # psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USERNAME" --password "$POSTGRESQL_PASSWORD" -d "$database" <<-EOSQL
+    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" <<-EOSQL
+
         DROP SCHEMA IF EXISTS public CASCADE;
 EOSQL
 
@@ -74,37 +74,37 @@ EOSQL
     #  Scripts necessários para criação da estrutura inicial do database.                           #
     #################################################################################################
     ## I. Criação da estrutura do banco de dados - Obrigatorio
-    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" -a -q -f /docker-entrypoint-initdb.d/pgd-sqls/a_criacao_estrutura_bd_obrigatorio.sql
+    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" -a -q -f "/usr/local/pgd-sqls/a_criacao_estrutura_bd_obrigatorio.sql"
 
     ## II. Inserir dados de domínio - Obrigatorio
-    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" -a -q -f /docker-entrypoint-initdb.d/pgd-sqls/b_inserir_dados_dominio_obrigatorio.sql
+    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" -a -q -f "/usr/local/pgd-sqls/b_inserir_dados_dominio_obrigatorio.sql"
 
     ## III. Criação da tabela pessoa alocacao temporaria - Obrigatorio
-    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" -a -q -f /docker-entrypoint-initdb.d/pgd-sqls/c_criacao_tabela_pessoa_alocacao_temp_obrigatorio.sql
+    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" -a -q -f "/usr/local/pgd-sqls/c_criacao_tabela_pessoa_alocacao_temp_obrigatorio.sql"
 
     ## IV. Alteracoes da estrutura do BD para a V7 - Correção de bugs
-    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" -a -q -f /docker-entrypoint-initdb.d/pgd-sqls/d_alteracoes_estrutura_bd_para_v7.sql
+    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" -a -q -f "/usr/local/pgd-sqls/d_alteracoes_estrutura_bd_para_v7.sql"
 
     ## V. Inserir dados de teste - Opcional ) (não necessário para produção)
-    #psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" -a -q -f "/docker-entrypoint-initdb.d/pgd-sqls/e_inserir_dados_teste_opcional.sql"
+    #psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" -a -q -f "/usr/local/pgd-sqls/e_inserir_dados_teste_opcional.sql"
 
     ## VI. Funcoes PostgreSQL - Obrigatorio
-    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" -a -q -f /docker-entrypoint-initdb.d/pgd-sqls/f_funcoes_postgresql_obrigatorio.sql
+    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" -a -q -f "/usr/local/pgd-sqls/f_funcoes_postgresql_obrigatorio.sql"
 
     #################################################################################################
     #  Scripts necessários para a instalação da API de envio dos dados de PGD para o órgão central. #
     #################################################################################################
     ## 1. Update Script 1 - Sob Demanda
-    #psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" -a -q -f "/docker-entrypoint-initdb.d/pgd-sqls/script1_create_tables_sql_server_susep.sql"
+    #psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" -a -q -f "/usr/local/pgd-sqls/script1_create_tables_sql_server_susep.sql"
 
     ## 2. Update Script 2 - Sob Demanda (versoes anteriores v1.7)
-    ## psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" -a -q -f "/docker-entrypoint-initdb.d/pgd-sqls/script2_vews_api_pgd_susep_versoes_anteriores_v7.sql"
+    ## psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" -a -q -f "/usr/local/pgd-sqls/script2_vews_api_pgd_susep_versoes_anteriores_v7.sql"
 
     ## 3. Update Script 2 - Sob Demanda (versoes v1.7+)
-    #psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" -a -q -f "/docker-entrypoint-initdb.d/pgd-sqls/script2_views_api_pgd_susep.sql"
+    #psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" -a -q -f "/usr/local/pgd-sqls/script2_views_api_pgd_susep.sql"
 
     ## Script extras - Sob Demanda (para listar pessoas versoes update v1.7+)
-    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRES_USER" -W "$POSTGRES_PASSWORD" -a -q -f /docker-entrypoint-initdb.d/pgd-sqls/script_comandos_sobdemanda.sql
+    psql -v ON_ERROR_STOP=1 -d "host=localhost port=5432 dbname=$database user=$POSTGRESQL_USERNAME" -W "$POSTGRESQL_PASSWORD" -a -q -f /usr/local/pgd-sqls/z_script_comandos_sobdemanda.sql
 }
 
 if [ "$( psql -XtAc "SELECT 1 FROM pg_database WHERE datname='pgd_staging'" )" = '1' ]
